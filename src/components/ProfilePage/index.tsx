d6136df0badabc0bf43968d2ed5ed9e5fcf03583
Guilherme Raposo <gui.raposo36@gmail.com>
2022-04-04 16:00:54 -0300

    feat: created component of ProfilePage

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+  Avatar,
+  Banner,
+  CakeIcon,
+  Container,
+  Followage,
+  LocationIcon,
+  ProfileData,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Guilherme Raposo</h1>
+        <h2>@guirapososilva</h2>
+
+        <p>Front-End Developer</p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+
+          <li>
+            <CakeIcon />
+            Nascido(a) em 17 de março de 2000
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>100</strong>
+          </span>
+          <span>
+            <strong>200 </strong>seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
